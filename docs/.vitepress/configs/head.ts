e0e0e91d5d66d048ad21bc1618d869e7bdf10dd7
vanhiupun <fanxiaobin422@gmail.com>
2023-10-09 22:46:35 +0800

    删除谷歌分析

--- docs/.vitepress/configs/head.ts
+++ docs/.vitepress/configs/head.ts
@@ -10,25 +10,6 @@ export const head: HeadConfig[] = [
   ["meta", { name: "og:locale", content: "cn" }],
   ["meta", { name: "og:site_name", content: "Theo Notes" }],
   ["meta", { name: "og:image", content: "https://theovan.xyz/avatar.png" }],
-
   // 强制开启为深色模式
   // ["script", {}, `document.documentElement.classList.add('dark')`],
-
-  // 谷歌分析
-  [
-    "script",
-    {
-      async: "",
-      src: "https://www.googletagmanager.com/gtag/js?id=G-2H93CC808K",
-    },
-  ],
-  [
-    "script",
-    {},
-    `window.dataLayer = window.dataLayer || [];
-      function gtag(){dataLayer.push(arguments);}
-      gtag('js', new Date());
-
-      gtag('config', 'G-2H93CC808K');`,
-  ],
 ];
