ec24ce1b73fa75804088aba69c1274975c8f5f3d
Theo <fanxiaobin422@gmail.com>
2024-05-16 00:25:05 +0800

    chore: add TypeScript Twoslash

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -1,16 +1,18 @@
 // .vitepress/theme/index.ts */
 import DefaultTheme from 'vitepress/theme'
-import Layout from './Layout.vue'
+import type { EnhanceAppContext } from 'vitepress'
+import TwoslashFloatingVue from '@shikijs/vitepress-twoslash/client'
 import googleAnalytics from 'vitepress-plugin-google-analytics'
+import Layout from './Layout.vue'
 
+import '@shikijs/vitepress-twoslash/style.css'
 import './styles/index.css'
 
 export default {
   extends: DefaultTheme,
-  enhanceApp: (ctx) => {
-    googleAnalytics({
-      id: 'G-6QN23XNMXB'
-    })
+  enhanceApp: ({ app }: EnhanceAppContext, ctx) => {
+    app.use(TwoslashFloatingVue)
+    googleAnalytics({ id: 'G-6QN23XNMXB' })
   },
   Layout
 }
