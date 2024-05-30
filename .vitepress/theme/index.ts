5d912d0a0aed0cfaaf05bef3af18eb1c831ec969
Theo <fanxiaobin422@gmail.com>
2024-05-30 19:45:23 +0800

    feat: add image click to enlarge function

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -1,11 +1,12 @@
 // .vitepress/theme/index.ts */
 import DefaultTheme from 'vitepress/theme'
-import type { EnhanceAppContext } from 'vitepress'
+import { EnhanceAppContext, useRoute } from 'vitepress'
 import TwoslashFloatingVue from '@shikijs/vitepress-twoslash/client'
 import googleAnalytics from 'vitepress-plugin-google-analytics'
 import { inject } from '@vercel/analytics'
 import AsideSponsors from './components/pendant.vue'
-import { h } from 'vue'
+import { h, onMounted, watch, nextTick } from 'vue'
+import mediumZoom from 'medium-zoom'
 
 import '@shikijs/vitepress-twoslash/style.css'
 import './styles/index.css'
@@ -22,5 +23,19 @@ export default {
   enhanceApp: ({ app }: EnhanceAppContext, ctx) => {
     app.use(TwoslashFloatingVue)
     googleAnalytics({ id: 'G-6QN23XNMXB' })
+  },
+  setup() {
+    const route = useRoute()
+    const initZoom = () => {
+      mediumZoom('[data-zoomable]', { background: 'var(--vp-c-bg)' })
+      mediumZoom('.main img', { background: 'var(--vp-c-bg)' })
+    }
+    onMounted(() => {
+      initZoom()
+    })
+    watch(
+      () => route.path,
+      () => nextTick(() => initZoom())
+    )
   }
 }
