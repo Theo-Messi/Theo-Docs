9c5c46fd56f817813df7dd16dbe9b75c220244ad
Theo <fanxiaobin422@gmail.com>
2024-06-14 20:14:06 +0800

    feat(theme): restructure the footer section to make it look more stylish

--- .vitepress/theme/components/Footer.vue
+++ .vitepress/theme/components/Footer.vue
@@ -4,171 +4,167 @@
     style="display: flex; flex-direction: column; align-items: center"
   >
     <div
-      class="flex flex-grow-1 ma4"
+      class="flex flex-grow-1 ma2"
       style="display: flex; justify-content: center; width: 100%"
     >
-      <div class="ma4">
+      <div class="ma2" style="text-align: center">
         <a title="Home" target="_blank" href="/">
           <img
             role="img"
-            width="36"
-            height="36"
-            viewBox="0 0 173 24"
+            width="48"
+            height="48"
             src="https://m.theovan.cn/avatar.png"
           />
         </a>
       </div>
+    </div>
 
-      <div
-        class="flex flex-grow-1 ma4"
-        style="display: flex; justify-content: center; width: 100%"
-      >
-        <!-- GitHub Source -->
-        <div class="fb6">
-          <div class="mb2 fw6 f14">GitHub Source</div>
-          <ul>
-            <li class="mb2">
-              <a
-                class="external-link"
-                target="_blank"
-                href="https://github.com/Theo-messi/Theo-Docs"
-                >Theo-Docs</a
-              >
-            </li>
-            <li class="mb2">
-              <a
-                class="external-link"
-                target="_blank"
-                href="https://github.com/Theo-messi/xx.theovan.cn"
-                >玄学宝典</a
-              >
-            </li>
-            <li class="mb2">
-              <a
-                class="external-link"
-                target="_blank"
-                href="https://github.com/Theo-messi/share.theovan.cn"
-                >阿里云盘资源分享</a
-              >
-            </li>
-            <li class="mb2">
-              <a
-                class="external-link"
-                target="_blank"
-                href="https://github.com/Theo-messi/own-rules"
-                >自用 Clash 配置</a
-              >
-            </li>
-          </ul>
-        </div>
-        <!-- 合作伙伴 -->
-        <div class="fb6">
-          <div class="mb2 fw6 f14">合作伙伴</div>
-          <ul>
-            <li class="mb2">
-              <a
-                class="external-link"
-                target="_blank"
-                href="https://nf.video/kaIuE"
-                >银河录像局</a
-              >
-            </li>
-            <li class="mb2">
-              <a
-                class="external-link"
-                target="_blank"
-                href="https://ihezu.love/UKTer6"
-                >奈飞小铺</a
-              >
-            </li>
-            <li class="mb2">
-              <a
-                class="external-link"
-                target="_blank"
-                href="https://fbinv01.fbaff.cc/auth/register?code=RZP3"
-                >FlyingBird</a
-              >
-            </li>
-            <li class="mb2">
-              <a
-                class="external-link"
-                target="_blank"
-                href="https://patriot.ninja/aff.php?aff=1471"
-                >Bridge The Wise</a
-              >
-            </li>
-            <li class="mb2">
-              <a
-                class="external-link"
-                target="_blank"
-                href="https://sms-activate.org/?ref=8170513"
-                >SMS-Activate</a
-              >
-            </li>
-          </ul>
-        </div>
-        <!-- 观影指南 -->
-        <div class="fb6">
-          <div class="mb2 fw6 f14">观影指南</div>
-          <ul>
-            <li class="mb2">
-              <a target="_blank" href="/streaming/Netflix-guide">Netflix</a>
-            </li>
-            <li class="mb2">
-              <a target="_blank" href="/streaming/Disney-introduce">Disney+</a>
-            </li>
-            <li class="mb2">
-              <a target="_blank" href="/streaming/Spotify">Spotify Premium</a>
-            </li>
-            <li class="mb2">
-              <a target="_blank" href="/streaming/YouTube">YouTube Premium</a>
-            </li>
-            <li class="mb2">
-              <a target="_blank" href="/streaming/Hulu">Hulu</a>
-            </li>
-            <li class="mb2">
-              <a target="_blank" href="/streaming/HBO-Max">HBO Max</a>
-            </li>
-          </ul>
-        </div>
-        <!-- 友情链接 -->
-        <div class="fb6">
-          <div class="mb2 fw6 f14">相关链接</div>
-          <ul>
-            <li class="mb2">
-              <a
-                class="external-link"
-                target="_blank"
-                href="https://theovan.cn/"
-                >Theo-Docs</a
-              >
-            </li>
-            <li class="mb2">
-              <a
-                class="external-link"
-                target="_blank"
-                href="https://xx.theovan.cn/"
-                >玄学宝典</a
-              >
-            </li>
-            <li class="mb2">
-              <a
-                class="external-link"
-                target="_blank"
-                href="https://share.theovan.cn/"
-                >阿里云盘资源分享</a
-              >
-            </li>
-            <li class="mb2">
-              <a
-                class="external-link"
-                target="_blank"
-                href="https://vitepress.dev/"
-                >VitePress</a
-              >
-            </li>
-          </ul>
-        </div>
+    <div
+      class="flex flex-grow-1 ma4"
+      style="display: flex; justify-content: center; width: 100%"
+    >
+      <!-- GitHub Source -->
+      <div class="fb6">
+        <div class="mb2 fw6 f14">GitHub Source</div>
+        <ul>
+          <li class="mb2">
+            <a
+              class="external-link"
+              target="_blank"
+              href="https://github.com/Theo-messi/Theo-Docs"
+              >Theo-Docs</a
+            >
+          </li>
+          <li class="mb2">
+            <a
+              class="external-link"
+              target="_blank"
+              href="https://github.com/Theo-messi/xx.theovan.cn"
+              >玄学宝典</a
+            >
+          </li>
+          <li class="mb2">
+            <a
+              class="external-link"
+              target="_blank"
+              href="https://github.com/Theo-messi/share.theovan.cn"
+              >阿里云盘资源分享</a
+            >
+          </li>
+          <li class="mb2">
+            <a
+              class="external-link"
+              target="_blank"
+              href="https://github.com/Theo-messi/own-rules"
+              >自用 Clash 配置</a
+            >
+          </li>
+        </ul>
+      </div>
+      <!-- 合作伙伴 -->
+      <div class="fb6">
+        <div class="mb2 fw6 f14">合作伙伴</div>
+        <ul>
+          <li class="mb2">
+            <a
+              class="external-link"
+              target="_blank"
+              href="https://nf.video/kaIuE"
+              >银河录像局</a
+            >
+          </li>
+          <li class="mb2">
+            <a
+              class="external-link"
+              target="_blank"
+              href="https://ihezu.love/UKTer6"
+              >奈飞小铺</a
+            >
+          </li>
+          <li class="mb2">
+            <a
+              class="external-link"
+              target="_blank"
+              href="https://fbinv01.fbaff.cc/auth/register?code=RZP3"
+              >FlyingBird</a
+            >
+          </li>
+          <li class="mb2">
+            <a
+              class="external-link"
+              target="_blank"
+              href="https://patriot.ninja/aff.php?aff=1471"
+              >Bridge The Wise</a
+            >
+          </li>
+          <li class="mb2">
+            <a
+              class="external-link"
+              target="_blank"
+              href="https://sms-activate.org/?ref=8170513"
+              >SMS-Activate</a
+            >
+          </li>
+        </ul>
+      </div>
+      <!-- 观影指南 -->
+      <div class="fb6">
+        <div class="mb2 fw6 f14">观影指南</div>
+        <ul>
+          <li class="mb2">
+            <a target="_blank" href="/streaming/Netflix-guide">Netflix</a>
+          </li>
+          <li class="mb2">
+            <a target="_blank" href="/streaming/Disney-introduce">Disney+</a>
+          </li>
+          <li class="mb2">
+            <a target="_blank" href="/streaming/Spotify">Spotify Premium</a>
+          </li>
+          <li class="mb2">
+            <a target="_blank" href="/streaming/YouTube">YouTube Premium</a>
+          </li>
+          <li class="mb2">
+            <a target="_blank" href="/streaming/Hulu">Hulu</a>
+          </li>
+          <li class="mb2">
+            <a target="_blank" href="/streaming/HBO-Max">HBO Max</a>
+          </li>
+        </ul>
+      </div>
+      <!-- 友情链接 -->
+      <div class="fb6">
+        <div class="mb2 fw6 f14">相关链接</div>
+        <ul>
+          <li class="mb2">
+            <a class="external-link" target="_blank" href="https://theovan.cn/"
+              >Theo-Docs</a
+            >
+          </li>
+          <li class="mb2">
+            <a
+              class="external-link"
+              target="_blank"
+              href="https://xx.theovan.cn/"
+              >玄学宝典</a
+            >
+          </li>
+          <li class="mb2">
+            <a
+              class="external-link"
+              target="_blank"
+              href="https://share.theovan.cn/"
+              >阿里云盘资源分享</a
+            >
+          </li>
+          <li class="mb2">
+            <a
+              class="external-link"
+              target="_blank"
+              href="https://vitepress.dev/"
+              >VitePress</a
+            >
+          </li>
+        </ul>
       </div>
     </div>
 
