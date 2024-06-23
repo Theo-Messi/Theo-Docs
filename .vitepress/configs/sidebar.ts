c47950bb839cb039228c896f7df08fe72c8c4a84
Theo <fanxiaobin422@gmail.com>
2024-06-23 19:04:01 +0800

    chore(docs): optimize server classification and add oracle operations

--- .vitepress/configs/sidebar.ts
+++ .vitepress/configs/sidebar.ts
@@ -1,37 +1,22 @@
-import { link } from 'fs'
 import type { DefaultTheme } from 'vitepress'
 
 // 侧边栏配置
 export const sidebar: DefaultTheme.Config['sidebar'] = {
-  // VPS使用指南
   '/vps/': { base: '/vps/', items: Sidebar_vps() },
-  // 华硕路由器固件
   '/asus/': { base: '/asus/', items: Sidebar_Asus() },
-  // ESXi安装指南
   '/ESXi/': { base: '/ESXi/', items: Sidebar_ESXi() },
-  // 青龙面板
-  '/vps/ql/': { base: '/vps/ql/', items: Sidebar_ql() },
-  // 入门指南
   '/FE/': { base: '/FE/', items: Sidebar_FE() },
-  // 文档记录
   '/notes/': { base: '/notes/', items: Sidebar_notes() },
-  // 服务推荐
   '/serve/': { base: '/serve/', items: Sidebar_serve() },
-  // 流媒体观影指南
   '/streaming/': { base: '/streaming/', items: Sidebar_streaming() }
 }
 
-// 文档记录
 export function Sidebar_notes() {
   return [
     {
-      // 分组名称
       text: '文档记录',
-      // 下拉开关
       // collapsed: true,
-      // 分组路径
       base: '/notes/',
-      // 分组页面
       items: [
         { text: 'PicGo + Github 搭建图床', link: 'build-picture-bed' },
         { text: 'GitHub Actions 工作流程', link: 'github-actions' },
@@ -46,13 +31,9 @@ export function Sidebar_notes() {
       ]
     },
     {
-      // 分组名称
       text: 'VitePress 搭建记录',
-      // 下拉开关
       // collapsed: true,
-      // 分组路径
       base: '/notes/vitepress/',
-      // 分组页面
       items: [
         { text: '添加 Giscu评论', link: 'giscu' },
         { text: '实现单击图片放大', link: 'Picture-enlargement' },
@@ -62,17 +43,12 @@ export function Sidebar_notes() {
   ]
 }
 
-// 华硕路由器固件
 export function Sidebar_Asus() {
   return [
     {
-      // 分组名称
       // text: '梅林操作文档',
-      // 下拉开关
       // collapsed: true,
-      // 分组路径
       base: '/asus/',
-      // 分组页面
       items: [
         { text: '刷机需知', link: 'Flashing-prerequisites' },
         { text: '固件平台介绍', link: 'Firmware-platform-introduction' },
@@ -85,17 +61,12 @@ export function Sidebar_Asus() {
   ]
 }
 
-// 入门指南
 export function Sidebar_FE() {
   return [
     {
-      // 分组名称
       text: 'Linux 文档',
-      // 下拉开关
       collapsed: true,
-      // 分组路径
       base: '/FE/Linux/',
-      // 分组页面
       items: [
         { text: 'Linux 目录结构', link: 'directory' },
         { text: 'Linux 文件操作', link: 'file' },
@@ -108,30 +79,21 @@ export function Sidebar_FE() {
       ]
     },
     {
-      // 分组名称
       text: 'Docker 文档',
-      // 下拉开关
       collapsed: true,
-      // 分组路径
       base: '/FE/Docker/',
-      // 分组页面
       items: [
         { text: 'Docker 简介与安装', link: 'install' },
         { text: 'Docker 进程相关操作', link: 'process' },
         { text: 'Docker 国内镜像加速', link: 'mirror-source' },
         { text: 'Docker 镜像相关操作', link: 'mirror' },
         { text: 'Docker 容器相关操作', link: 'container' }
-        // { text: 'HomeBrew 切换镜像源', link: 'mirror-source' }
       ]
     },
     {
-      // 分组名称
       text: 'Git 文档',
-      // 下拉开关
       collapsed: true,
-      // 分组路径
       base: '/FE/Git/',
-      // 分组页面
       items: [
         { text: 'Git 简介与安装', link: 'install' },
         { text: 'Git 设置用户信息', link: 'user' },
@@ -143,13 +105,9 @@ export function Sidebar_FE() {
       ]
     },
     {
-      // 分组名称
       text: 'HomeBrew 文档',
-      // 下拉开关
       collapsed: true,
-      // 分组路径
       base: '/FE/HomeBrew/',
-      // 分组页面
       items: [
         { text: 'HomeBrew 简介与安装', link: 'install' },
         { text: 'HomeBrew 安装软件', link: 'software' },
@@ -158,31 +116,22 @@ export function Sidebar_FE() {
       ]
     },
     {
-      // 分组名称
       text: 'Vercel-CLI 文档',
       link: 'Vercel-CLI',
-      // 下拉开关
       collapsed: true,
-      // 分组路径
       base: '/FE/',
       items: []
     },
     {
-      // 分组名称
       text: 'Markdown 演示',
       link: 'Markdown',
-      // 下拉开关
       collapsed: true,
-      // 分组路径
       base: '/FE/',
       items: []
     },
     {
-      // 分组名称
       text: 'pnpm 文档',
-      // 下拉开关
       collapsed: true,
-      // 分组路径
       base: '/FE/pnpm/',
       items: [
         { text: 'pnpm 安装', link: 'install' },
@@ -192,22 +141,15 @@ export function Sidebar_FE() {
     }
   ]
 }
-// vps使用指南
+
 export function Sidebar_vps() {
   return [
     {
-      // 分组名称
       text: 'Debian/Ubuntu',
-      // 下拉开关
-      // collapsed: true,
-      // 分组路径
-      base: '/vps/',
-      // 分组页面
+      collapsed: true,
+      base: '/vps/settings/',
       items: [
-        {
-          text: '系统优化设置',
-          link: 'Ubuntu'
-        },
+        { text: '系统优化设置', link: 'Ubuntu' },
         { text: '更改中文语言', link: 'Chinese' },
         { text: '开启防火墙端口', link: 'firewall-port' },
         { text: 'Debian12 安装使用 Cloud 内核', link: 'Debian12-Cloud' },
@@ -216,13 +158,9 @@ export function Sidebar_vps() {
       ]
     },
     {
-      // 分组名称
       text: '系统工具',
-      // 下拉开关
-      // collapsed: true,
-      // 分组路径
-      base: '/vps/',
-      // 分组页面
+      collapsed: true,
+      base: '/vps/tools/',
       items: [
         { text: '全能工具箱', link: 'All-purpose-toolbox' },
         { text: 'dd.sh 重装系统', link: 'dd.sh' },
@@ -233,13 +171,9 @@ export function Sidebar_vps() {
       ]
     },
     {
-      // 分组名称
       text: '搭建服务',
-      // 下拉开关
-      // collapsed: true,
-      // 分组路径
-      base: '/vps/',
-      // 分组页面
+      collapsed: true,
+      base: '/vps/serve/',
       items: [
         { text: 'X-ui', link: 'X-ui' },
         { text: 'V2Ray', link: 'V2Ray' },
@@ -249,6 +183,51 @@ export function Sidebar_vps() {
         { text: 'Docker 搭建 aria2+AriaNg', link: 'aria2+AriaNg' },
         { text: '一键安装 小雅Xiaoya全家桶', link: 'xiaoya-alist' }
       ]
+    },
+    {
+      text: 'Oracle Cloud 甲骨文云',
+      collapsed: true,
+      base: '/vps/oracle-cloud/',
+      items: [
+        { text: '甲骨文云服务器 开启IPv6', link: 'IPv6' },
+        { text: 'Debian系统自动获取IPv6', link: 'Debian-IPv6' }
+      ]
+    },
+    {
+      text: '青龙面板',
+      collapsed: true,
+      base: '/vps/ql/',
+      items: [
+        {
+          items: [
+            { text: '安装青龙面板', link: 'Install-Qinglong-panel' },
+            { text: '阿里云盘签到', link: 'Alipan-sign-in' },
+            { text: '京东签到', link: 'JD-sign-in' },
+            { text: 'BiliBili 签到', link: 'BiliBili-sign-in' }
+          ]
+        },
+        {
+          text: '获取京东Cookie',
+          items: [
+            { text: '插件版', link: 'JD-Cookie-plug-in' },
+            { text: '浏览器版', link: 'JD-Cookie-Browser' },
+            { text: 'Boxjs', link: 'JD-Cookie-Boxjs' }
+          ]
+        },
+        {
+          text: '获取阿里云盘Cookie',
+          items: [
+            {
+              text: '自动获取 - refresh_token',
+              link: 'Alipan-Get-cookies-automatically'
+            },
+            {
+              text: '手动获取 - refresh_token',
+              link: 'Alipan-Get-cookies-manually'
+            }
+          ]
+        }
+      ]
     }
   ]
 }
@@ -257,13 +236,9 @@ export function Sidebar_vps() {
 export function Sidebar_ESXi() {
   return [
     {
-      // 分组名称
       text: 'ESXi 8.0 部署指南',
-      // 下拉开关
       // collapsed: true,
-      // 分组路径
       base: '/ESXi/Install/',
-      // 分组页面
       items: [
         { text: 'ESXi 安装指南', link: 'guide' },
         { text: '安装 Openwrt', link: 'Openwrt' },
@@ -272,13 +247,9 @@ export function Sidebar_ESXi() {
       ]
     },
     {
-      // 分组名称
       text: 'OpenWrt/iStoreOS',
-      // 下拉开关
       // collapsed: true,
-      // 分组路径
       base: '/ESXi/openwrt/',
-      // 分组页面
       items: [
         { text: 'iStore 插件包', link: 'iStore-plugin-package' },
         { text: 'PPPOE拨号', link: 'PPPOE-dial-up' },
@@ -295,70 +266,13 @@ export function Sidebar_ESXi() {
   ]
 }
 
-// 青龙面板
-export function Sidebar_ql() {
-  return [
-    {
-      // 分组名称
-      text: '安装青龙面板',
-      // 下拉开关
-      // collapsed: true,
-      // 分组路径
-      base: '/vps/ql/',
-      // 分组页面
-      items: [
-        { text: '安装 青龙面板', link: 'Install-Qinglong-panel' },
-        { text: '阿里云盘签到', link: 'Alipan-sign-in' },
-        { text: '京东签到', link: 'JD-sign-in' },
-        { text: 'BiliBili 签到', link: 'BiliBili-sign-in' }
-      ]
-    },
-    {
-      // 分组名称
-      text: '获取京东Cookie',
-      // 下拉开关
-      // collapsed: true,
-      // 分组路径
-      base: '/vps/ql/',
-      //分组页面
-      items: [
-        { text: '插件版', link: 'JD-Cookie-plug-in' },
-        { text: '浏览器版', link: 'JD-Cookie-Browser' },
-        { text: 'Boxjs', link: 'JD-Cookie-Boxjs' }
-      ]
-    },
-    {
-      // 分组名称
-      text: '获取阿里云盘Cookie',
-      // 下拉开关
-      // collapsed: true,
-      // 分组路径
-      base: '/vps/ql/',
-      // 分组页面
-      items: [
-        {
-          text: '自动获取 - refresh_token',
-          link: 'Alipan-Get-cookies-automatically'
-        },
-        {
-          text: '手动获取 - refresh_token',
-          link: 'Alipan-Get-cookies-manually'
-        }
-      ]
-    }
-  ]
-}
 // 流媒体观影指南
 export function Sidebar_streaming() {
   return [
     {
-      // 分组名称
       // text: 'Netflix 观看指南',
-      // 下拉菜单
       // collapsed: true,
-      // 分组路径
       base: '/streaming/',
-      // 分组页面
       items: [
         {
           text: `Netflix 观看指南`,
@@ -383,40 +297,28 @@ export function Sidebar_streaming() {
 export function Sidebar_serve() {
   return [
     {
-      // 分组名称
       text: '账号合租平台',
       link: 'sharing/Account-sharing-guide',
-      // 下拉菜单
       // collapsed: true,
-      // 分组路径
       base: '/serve/',
-      // 分组页面
       items: [
         { text: '银河录像局', link: 'sharing/nf-video' },
         { text: '奈飞小铺', link: 'sharing/ihezu' }
       ]
     },
     {
-      // 分组名称
       text: '优质机场',
-      // 下拉菜单
       // collapsed: true,
-      // 分组路径
       base: '/serve/',
-      // 分组页面
       items: [
         { text: 'FlyingBird - 飞鸟机场', link: 'airport/FlyingBird' },
         { text: 'Bridge the Wise', link: 'airport/Bridge-the-Wise' }
       ]
     },
     {
-      // 分组名称
       text: '接码平台',
-      // 下拉菜单
       // collapsed: true,
-      // 分组路径
       base: '/serve/',
-      // 分组页面
       items: [{ text: 'SMS-Activate', link: 'code/SMS-Activate' }]
     }
   ]
