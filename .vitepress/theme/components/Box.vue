7fdc866ae8b14db58a8ffe0790f630d698a393fa
Theo <fanxiaobin422@gmail.com>
2024-06-29 20:32:45 +0800

    fix(component): remove redundant classes

--- .vitepress/theme/components/Box.vue
+++ .vitepress/theme/components/Box.vue
@@ -19,28 +19,71 @@ export default {
   }
 }
 </script>
+
 <template>
-  <div class="boxs-container block">
-    <a v-for="(item, index) in items" :key="index" :href="item.l" class="boxs" target="_blank">
-      <div class="block-link">
+  <div class="box-container">
+    <a v-for="(item, index) in items" :key="index" :href="item.l" class="box" target="_blank">
+      <div class="box-content">
         <span v-if="item.ct" class="ct">{{ item.ct }}</span>
-        <img v-if="item.i" :src="item.i" alt="" class="block-icon" />
-        <p>{{ item.t }}</p>
+        <img v-if="item.i" :src="item.i" alt="" class="i" />
+        <p class="t">{{ item.t }}</p>
       </div>
     </a>
   </div>
 </template>
 
 <style lang="scss" scoped>
-.block {
+.box-container {
+  display: flex;
+  flex-wrap: wrap;
+  gap: 20px;
   margin-top: 1rem;
 }
+
+.box {
+  position: relative;
+  border: 1px solid var(--vp-c-bg-soft);
+  background-color: var(--vp-c-bg-alt);
+  padding: 12px 24px;
+  border-radius: 8px;
+  height: 60px;
+  width: 210px;
+  display: flex;
+  transition: all 0.3s cubic-bezier(0.25, 0.8, 0.25, 1);
+
+  &:hover {
+    border-color: var(--vp-c-brand-1);
+    // background-color: var(--vp-c-brand-soft);
+  }
+
+  @media (max-width: 1024px) {
+    flex: 1 1 calc(50% - 20px);
+    max-width: calc(50% - 20px);
+  }
+
+  @media (max-width: 768px) {
+    flex: 1 1 100%;
+    max-width: 100%;
+  }
+}
+
+.box-content {
+  font-size: 14px;
+  line-height: 1;
+  letter-spacing: -0.02em;
+  display: flex;
+  align-items: center;
+  text-decoration: none;
+  overflow: hidden;
+  white-space: nowrap;
+}
+
 .ct {
   position: absolute;
   top: 0;
   right: 0;
   background-color: var(--vp-c-brand-3);
-  color: var(--c-text-light-1);
+  color: var(--vp-c-brand-text);
   font-size: 10px;
   padding: 4px 8px;
   border-top-right-radius: 8px;
@@ -48,46 +91,7 @@ export default {
   z-index: 1;
 }
 
-.block .boxs {
-  position: relative;
-  border: 1px solid var(--vp-c-bg-soft);
-  background-color: var(--vp-c-bg-alt);
-  padding: 12px 24px;
-  border-radius: 8px;
-  font-size: 12px;
-  font-weight: 500;
-  text-decoration: none;
-  height: 60px;
-  width: 210px;
-  display: flex;
-  align-items: center;
-  overflow: hidden;
-  transition:
-    border-color 0.3s cubic-bezier(0.25, 0.8, 0.25, 1),
-    color 0.5s,
-    background-color 0.5s;
-}
-
-.block .boxs:hover {
-  border-color: var(--vp-c-brand-1);
-  /* background-color: var(--vp-c-brand-soft); */
-  text-decoration: none;
-}
-
-.block-link {
-  font-size: 14.5px;
-  line-height: 1;
-  letter-spacing: -0.02em;
-  display: flex;
-  align-items: center;
-  color: var(--vp-c-brand-1);
-  text-decoration: none;
-  overflow: hidden;
-  text-overflow: ellipsis;
-  white-space: nowrap;
-}
-
-.block-icon {
+.i {
   margin-right: 8px;
   height: 2em;
   width: auto;
@@ -97,28 +101,15 @@ export default {
   border-radius: 6px;
 }
 
-.boxs-container {
+.t {
+  font-size: 14px;
+  line-height: 1;
+  letter-spacing: -0.02em;
   display: flex;
-  flex-wrap: wrap;
-  gap: 20px;
-}
-
-.boxs-container .boxs {
-  flex: 1 1 calc(33.333% - 20px);
-  max-width: 210px;
-}
-
-@media (max-width: 1024px) {
-  .boxs-container .boxs {
-    flex: 1 1 calc(50% - 20px);
-    max-width: calc(50% - 20px);
-  }
-}
-
-@media (max-width: 768px) {
-  .boxs-container .boxs {
-    flex: 1 1 100%;
-    max-width: 100%;
-  }
+  align-items: center;
+  text-decoration: none;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
 }
 </style>
