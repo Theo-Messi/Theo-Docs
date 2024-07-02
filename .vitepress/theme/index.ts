486a9ee4fb3fc6014fe9a3547d04793b6bb5485b
Theo <fanxiaobin422@gmail.com>
2024-07-02 12:00:15 +0800

    feat: optimize theme configuration

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -1,13 +1,13 @@
-// .vitepress/theme/index.ts */
 import DefaultTheme from 'vitepress/theme'
 import { EnhanceAppContext, useRoute } from 'vitepress'
+import { h } from 'vue'
+import { inject } from '@vercel/analytics'
+
 import TwoslashFloatingVue from '@shikijs/vitepress-twoslash/client'
 import googleAnalytics from 'vitepress-plugin-google-analytics'
-import { inject } from '@vercel/analytics'
 import AsideLogo from './components/AsideLogo.vue'
 import Box from './components/Box.vue'
 import Links from './components/Links.vue'
-import { h } from 'vue'
 import { NolebaseGitChangelogPlugin } from '@nolebase/vitepress-plugin-git-changelog/client'
 import imageViewer from 'vitepress-plugin-image-viewer'
 import vImageViewer from 'vitepress-plugin-image-viewer/lib/vImageViewer.vue'
@@ -19,6 +19,19 @@ import 'viewerjs/dist/viewer.min.css'
 import '@fortawesome/fontawesome-free/css/all.css'
 
 inject()
+const mapAuthors = [
+  {
+    name: 'Theo-Messi',
+    username: 'Theo-Messi',
+    mapByNameAliases: ['Theo', 'vanhiupun'],
+    mapByEmailAliases: ['fanxiaobin422@gmail.com', 'fanxiaobin422@sina.com', '771720405@qq.com']
+  },
+  {
+    name: 'dependabot',
+    username: 'dependabot',
+    mapByEmailAliases: ['49699333+dependabot[bot]@users.noreply.github.com']
+  }
+]
 
 export default {
   extends: DefaultTheme,
@@ -28,31 +41,15 @@ export default {
     })
   },
   enhanceApp: ({ app }: EnhanceAppContext, ctx) => {
-    app.use(TwoslashFloatingVue)
     googleAnalytics({ id: 'G-6QN23XNMXB' })
     app.component('vImageViewer', vImageViewer)
     app.component('Box', Box)
     app.component('Links', Links)
-    app.use(NolebaseGitChangelogPlugin, {
-      mapAuthors: [
-        {
-          name: 'Theo',
-          username: 'Theo-messi',
-          mapByNameAliases: ['Theo', 'vanhiupun'],
-          mapByEmailAliases: ['fanxiaobin422@gmail.com', 'fanxiaobin422@sina.com', '771720405@qq.com']
-        },
-        {
-          name: 'dependabot',
-          username: 'dependabot',
-          mapByEmailAliases: ['49699333+dependabot[bot]@users.noreply.github.com']
-        }
-      ]
-    })
+    app.use(TwoslashFloatingVue)
+    app.use(NolebaseGitChangelogPlugin, { mapAuthors })
   },
   setup() {
-    // 获取路由
     const route = useRoute()
-    // 使用
     imageViewer(route)
   }
 }
