64b4d27696137031f0422f6ed617662900464d9a
Theo <fanxiaobin422@gmail.com>
2024-08-26 10:39:51 +0800

    chore: bump deps

---
+++ .vitepress/data/FooterData.ts
@@ -0,0 +1,86 @@
+export const Footer_Data = {
+  beian: {
+    icp: '鄂ICP备2024060426号',
+    police: '粤公网安备44200102445449号'
+  },
+  author: {
+    name: 'Theo-Messi',
+    link: 'https://github.com/Theo-Messi',
+    time: 2019
+  },
+  group: [
+    {
+      title: 'GitHub Source',
+      links: [
+        {
+          name: 'Theo-Docs',
+          href: 'https://github.com/Theo-Messi/Theo-Docs'
+        },
+        {
+          name: '玄学宝典',
+          href: 'https://github.com/Theo-Messi/xx.theojs.cn'
+        },
+        {
+          name: '阿里云盘资源分享',
+          href: 'https://github.com/Theo-Messi/share.theojs.cn'
+        },
+        {
+          name: '常用配置文件',
+          href: 'https://github.com/Theo-Messi/dotfiles'
+        },
+        {
+          name: 'Lumen',
+          href: 'https://github.com/Theo-Messi/tools'
+        }
+      ]
+    },
+    {
+      title: '合作伙伴',
+      links: [
+        {
+          name: '🔥 青云梯',
+          href: 'https://qytcc01.qingyunti.pro/register?aff=jjgD79Jd'
+        },
+        { name: '银河录像局', href: 'https://nf.video/kaIuE' },
+        { name: '奈飞小铺', href: 'https://ihezu.love/UKTer6' },
+        {
+          name: 'FlyingBird',
+          href: 'https://fbinv01.fbaff.cc/auth/register?code=RZP3'
+        },
+        {
+          name: 'Bridge The Wise',
+          href: 'https://patriot.ninja/aff.php?aff=1471'
+        },
+        {
+          name: '飞兔云',
+          href: 'https://xn--9kq10e0y7h.site/index.html?register=2cFF8mg4'
+        },
+        {
+          name: 'SMS-Activate',
+          href: 'https://sms-activate.org/?ref=8170513'
+        }
+      ]
+    },
+    {
+      title: '观影指南',
+      internal: true,
+      links: [
+        { name: 'Netflix', href: '/streaming/Netflix-guide' },
+        { name: 'Disney+', href: '/streaming/Disney-introduce' },
+        { name: 'Spotify Premium', href: '/streaming/Spotify' },
+        { name: 'YouTube Premium', href: '/streaming/YouTube' },
+        { name: 'Hulu', href: '/streaming/Hulu' },
+        { name: 'HBO Max', href: '/streaming/HBO-Max' }
+      ]
+    },
+    {
+      title: '相关链接',
+      links: [
+        { name: 'Theo-Docs', href: 'https://doc.theojs.cn/' },
+        { name: '玄学宝典', href: 'https://xx.theojs.cn/' },
+        { name: '阿里云盘资源分享', href: 'https://share.theojs.cn/' },
+        { name: 'VitePress', href: 'https://vitepress.dev/' }
+      ]
+    }
+  ]
+}
