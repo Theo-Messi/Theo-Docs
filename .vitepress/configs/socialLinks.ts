144994f460bf1f8e4d4ef65b2271aa7b464c3da7
Theo <fanxiaobin422@gmail.com>
2024-09-01 19:23:37 +0800

    chore: The event has expired

---
+++ .vitepress/configs/socialLinks.ts
@@ -0,0 +1,27 @@
+import type { DefaultTheme } from 'vitepress'
+// 社交链接设置
+
+export const socialLinks: DefaultTheme.Config['socialLinks'] = [
+  {
+    icon: { svg: '<i class="fab fa-github fa-lg" alt="Github"></i>' },
+    link: 'https://github.com/Theo-Messi/Theo-Docs',
+    ariaLabel: 'Github'
+  },
+  {
+    icon: { svg: '<i class="fas fa-envelope fa-lg" alt="Gmail"></i>' },
+    link: 'mailto:fanxiaobin422@gmail.com',
+    ariaLabel: 'Gmail'
+  },
+  {
+    icon: { svg: '<i class="fab fa-telegram fa-lg" alt="Telegram"></i>' },
+    link: 'https://t.me/Theo_Messi',
+    ariaLabel: 'Telegram'
+  },
+  {
+    icon: {
+      svg: '<i class="fas fa-heart fa-lg" style="color: #EA4AAA;" alt="Github Sponsors"></i>'
+    },
+    link: 'https://github.com/sponsors/Theo-Messi',
+    ariaLabel: 'Github Sponsors'
+  }
+]
